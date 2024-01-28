26df359376bcbece302eb22da3ed88532983d961
KikoShaka <119086326+KikoShaka@users.noreply.github.com>
2024-01-28 11:14:41 +0300

    Update Counting Elements.cpp

--- Hashing/Counting problems/Counting Elements.cpp
+++ Hashing/Counting problems/Counting Elements.cpp
@@ -1,7 +1,6 @@
 //Given an integer array arr, count how many 
 //elements x there are, such that x + 1 is also in arr. 
 //If there are duplicates in arr, count them separately.
-
 int countElements(vector<int>& arr) {
         unordered_set<int> us(arr.begin(), arr.end());
         int count=0;
