a5f41cd5211240de2b5abce3e2978a78c2ff4e4f
KikoShaka <119086326+KikoShaka@users.noreply.github.com>
2024-01-28 10:07:05 +0300

    Update Checking for existence.cpp

--- Hashing/Checking for existence.cpp
+++ Hashing/Checking for existence.cpp
@@ -1,4 +1,5 @@
-//Given an array of integers nums and an integer target, return indices of two numbers such that they add up to target. You cannot use the same index twice.
+//Given an array of integers nums and an integer target, return indices
+//of two numbers such that they add up to target. You cannot use the same index twice.
 
 #include <iostream>
 #include <vector>
